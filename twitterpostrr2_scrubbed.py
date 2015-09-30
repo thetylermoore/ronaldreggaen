1255a556df4c1ca29fd01f7d3a94082cb32888ab
thetylermoore <tmoore@mntmoore.local>
2015-09-30 09:06:47 -0700

    Added Trending Hashtag
    Bot now ends tweet with local trending hashtag (for los angeles) to
    increase interactions.

--- twitterpostrr2_scrubbed.py
+++ twitterpostrr2_scrubbed.py
@@ -1,12 +1,10 @@
 from twitter import *
 import random
 
-#fill in the required information for entries marked "put_yours_here"
-
-access_token = "put_yours_here"
-access_token_secret = "put_yours_here"
-consumer_key = "put_yours_here"
-consumer_secret = "put_yours_here"
+access_token = "redacted"
+access_token_secret = "redacted"
+consumer_key = "redacted"
+consumer_secret = "redacted"
 
 t = Twitter(auth=OAuth(access_token, access_token_secret, consumer_key, consumer_secret))
 
@@ -24,55 +22,67 @@ marley = ['Jah',
           'Earth',
           'Tribes']
 
-reagan1_1 = " is not absense of "
-reagan1_2 = ", it is the ability to handle "
-reagan1_3 = " by means of "
+reagan0_1 = " is not absense of "
+reagan0_2 = ", it is the ability to handle "
+reagan0_3 = " by means of "
 
-reagan2_1 = " can't help everone, but everyone can help "
+reagan1_1 = " can't help everone, but everyone can help "
 
-reagan3_1 = "'s first duty is to protect the "
-reagan3_2 = ", not run their "
+reagan2_1 = "'s first duty is to protect the "
+reagan2_2 = ", not run their "
 
-reagan4_1 = " are never defeated unless we give up on "
+reagan3_1 = " are never defeated unless we give up on "
 
-reagan5_1 = "When you make them see the "
-reagan5_2 = ", make them feel the "
+reagan4_1 = "When you make them see the "
+reagan4_2 = ", make them feel the "
 
+t = Twitter(auth=OAuth(access_token, access_token_secret, consumer_key, consumer_secret))
+
+results = t.trends.place(_id = 2442047)
+#top 10 trending results from los angeles
+
+for location in results:
+    toptrend = location["trends"][0]['name']
+
+#module = 0
+#module = random.randint(0, 2)
 module = random.randint(0, 4)
 
 if module == 0:
     t.statuses.update(status=((marley[random.randint(0, len(marley) -1)]) +
-    #reagan1[random.randint(0, len(reagan1) -1)]
-    reagan1_1 +
-    (marley[random.randint(0, len(marley) -1)]) +
-    reagan1_2 +
-    (marley[random.randint(0, len(marley) -1)]) +
-    reagan1_3 +
-    (marley[random.randint(0, len(marley) -1)]) + '.'))
+                              reagan0_1 +
+                              (marley[random.randint(0, len(marley) -1)]) +
+                              reagan0_2 +
+                              (marley[random.randint(0, len(marley) -1)]) +
+                              reagan0_3 +
+                              (marley[random.randint(0, len(marley) -1)]) + '.' + ' #reggae' + ' #conservative ' + toptrend))
 
 elif module == 1:
     t.statuses.update(status=((marley[random.randint(0, len(marley) -1)]) +
-    reagan2_1 +
-    (marley[random.randint(0, len(marley) -1)]) + '.'))
+                              reagan1_1 +
+                              (marley[random.randint(0, len(marley) -1)]) + '.' + ' #reggae' + ' #conservative ' + toptrend))
 
 elif module == 2:
     t.statuses.update(status=((marley[random.randint(0, len(marley) -1)]) +
-    reagan3_1 +
-    (marley[random.randint(0, len(marley) -1)]) +
-    reagan3_2 +
-    (marley[random.randint(0, len(marley) -1)]) + '.'))
+                              reagan2_1 +
+                              (marley[random.randint(0, len(marley) -1)]) +
+                              reagan2_2 +
+                              (marley[random.randint(0, len(marley) -1)]) + '.' + ' #reggae' + ' #conservative ' + toptrend))
 
 elif module == 3:
     t.statuses.update(status=((marley[random.randint(0, len(marley) -1)]) +
-    reagan4_1 +
-    (marley[random.randint(0, len(marley) -1)]) + '.'))
+                              reagan3_1 +
+                              (marley[random.randint(0, len(marley) -1)]) + '.' + ' #reggae' + ' #conservative ' + toptrend))
 
 elif module == 4:
-    t.statuses.update(status=((reagan5_1 +
-    (marley[random.randint(0, len(marley) -1)]) +
-    reagan5_2 +
-    (marley[random.randint(0, len(marley) -1)]) + '.' + '#reggae ' + '#conservative'))
+    t.statuses.update(status=(
+                              reagan4_1 +
+                              (marley[random.randint(0, len(marley) -1)]) +
+                              reagan4_2 +
+                              (marley[random.randint(0, len(marley) -1)]) + '.' + ' #reggae' + ' #conservative ' + toptrend))
 
-#t.statuses.update(status=((marley[random.randint(0, len(marley) -1)]) +
-#    reagan[random.randint(0, len(reagan) -1)]
-#      + (marley[random.randint(0, len(marley) -1)]) + '. ' + '#reggae ' + '#conservative'))
+#elif module == 4:
+#    t.statuses.update(status=((reagan5_1 +
+#    (marley[random.randint(0, len(marley) -1)]) +
+#    reagan5_2 +
+#    (marley[random.randint(0, len(marley) -1)]) + '.' + ' #reggae' + ' #conservative'))
